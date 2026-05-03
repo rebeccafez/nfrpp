54062323682ce503a4da0324d799be67910dcecd
Rebecca Fessenden <rebecca@rebeccafessenden.com>
2026-05-03 11:36:22 -0400

    Add database connection info to configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,11 +5,19 @@ dotenv.config()
 interface Config {
     port: number;
     nodeEnv: string;
+    postgresHost: string;
+    postgresPort: number;
+    postgresUser: string;
+    postgresPassword: string;
 }
 
 const config: Config = {
     port: Number(process.env['PORT']) || 4000,
     nodeEnv: process.env['NODE_ENV'] || 'development',
+    postgresHost: process.env['POSTGRES_HOST'] || 'localhost',
+    postgresPort: Number(process.env['POSTGRES_PORT']) || 5432,
+    postgresUser: process.env['POSTGRES_USER'] || 'postgres',
+    postgresPassword: process.env['POSTGRES_PASSWORD'] || '',
 };
 
 export default config
