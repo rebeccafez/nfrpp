3b31448df4f85b26cf036f556f38ee4a439ced42
Rebecca Fessenden <92482068+rebeccafez@users.noreply.github.com>
2026-05-03 12:03:31 -0400

    feat: add database (#4)
    * Add database connection info to configuration
    
    * Add pg-promise dependency
    
    * Add postgres database option to config
    
    * Add database connection logic

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,11 +5,21 @@ dotenv.config()
 interface Config {
     port: number;
     nodeEnv: string;
+    postgresHost: string;
+    postgresPort: number;
+    postgresUser: string;
+    postgresPassword: string;
+    postgresDb: string;
 }
 
 const config: Config = {
     port: Number(process.env['PORT']) || 4000,
     nodeEnv: process.env['NODE_ENV'] || 'development',
+    postgresHost: process.env['POSTGRES_HOST'] || 'localhost',
+    postgresPort: Number(process.env['POSTGRES_PORT']) || 5432,
+    postgresUser: process.env['POSTGRES_USER'] || 'postgres',
+    postgresPassword: process.env['POSTGRES_PASSWORD'] || '',
+    postgresDb: process.env['POSTGRES_DB'] || 'postgres',
 };
 
 export default config
